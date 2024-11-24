5a04f122c1de43d29fab56727728a3042f01aee8
nofreewilljackson <hello@wujackson.com>
2024-11-23 22:10:52 -0600

    fix: fixed custom node package

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import Credit from "./components/Credit";
 import HeroSection from './components/WallpaperOverlay/HeroSection';
 import DefinitionSection from './components/WallpaperOverlay/DefinitionSection';
 import NavbarComp from "./components/NavbarComp";
-import FlashcardApp  from "./components/ExternalComps/FlashcardApp";
+import FlashcardApp from "./components/ExternalComps/FlashcardApp";
 import SWECards from "./components/ExternalComps/SWE";
 
 import { useState } from "react";
@@ -40,7 +40,7 @@ function App() {
   const handleOpenRecycleBin = () => toggleShowRecycleBin(true);
   const handleOpenCredit = () => toggleShowCredit(true);
   const handleOpenBlog = () => {
-    window.location.href = 'https://www.malleablemind.ai/j-garden/';
+    window.location.href = 'https://www.wujackson.com/jackson';
   };
   const handleOpenFlashcard = () => toggleShowFlashcardApp(true);
   const handleOpenSWECards = () => toggleShowSWECards(true);
@@ -48,64 +48,64 @@ function App() {
   const handleCloseHelp = () => toggleShowHelp(false);
 
   return (
-      <>
-        {/* Pass the state handlers to NavbarComp */}
-        <NavbarComp
-            openBlog={handleOpenBlog}
-            openResume={handleOpenResume}
-            openCoding={handleOpenCoding}
-            openPaint={handleOpenPaint}
+    <>
+      {/* Pass the state handlers to NavbarComp */}
+      <NavbarComp
+        openBlog={handleOpenBlog}
+        openResume={handleOpenResume}
+        openCoding={handleOpenCoding}
+        openPaint={handleOpenPaint}
+      />
+
+      <WindowSizeProvider>
+        <TaskBarComp
+          openPaint={handleOpenPaint}
+          openStreaming={handleOpenStreaming}
+          openArts={handleOpenArtsAndCrafts}
+          openResume={handleOpenResume}
+          openContact={handleOpenContact}
+          openCoding={handleOpenCoding}
+          openCredit={handleOpenCredit}
         />
+        <Desktop
+          openPaint={handleOpenPaint}
+          openStreaming={handleOpenStreaming}
+          openArts={handleOpenArtsAndCrafts}
+          openResume={handleOpenResume}
+          openContact={handleOpenContact}
+          openCoding={handleOpenCoding}
+          openRecycleBin={handleOpenRecycleBin}
+          openFlashcardApp={handleOpenFlashcard}
+          openSWECards={handleOpenSWECards}
+        />
+        <PaintComp
+          show={showPaint}
+          toggle={toggleShowPaint}
+          onHelp={handleShowHelp}
+        />
+        <StreamingComp show={showStreaming} toggle={toggleShowStreaming} />
+        <ArtsAndCrafts
+          show={showArtsAndCrafts}
+          toggle={toggleShowArtsAndCrafts}
+        />
+        <Resume show={showResume} toggle={toggleShowResume} />
+        <Contact show={showContact} toggle={toggleShowContact} />
+        <Coding show={showCoding} toggle={toggleShowCoding} />
+        <Credit show={showCredit} toggle={toggleShowCredit} />
+        <Credit show={showCredit} toggle={toggleShowCredit} />
+        <SWECards show={showSWECards} toggle={toggleShowSWECards} />
+        <FlashcardApp show={showFlashcardApp} toggle={toggleShowFlashcardApp} />
+        <RecycleBin show={showRecycleBin} toggle={toggleShowRecycleBin} />
 
-        <WindowSizeProvider>
-          <TaskBarComp
-              openPaint={handleOpenPaint}
-              openStreaming={handleOpenStreaming}
-              openArts={handleOpenArtsAndCrafts}
-              openResume={handleOpenResume}
-              openContact={handleOpenContact}
-              openCoding={handleOpenCoding}
-              openCredit={handleOpenCredit}
-          />
-          <Desktop
-              openPaint={handleOpenPaint}
-              openStreaming={handleOpenStreaming}
-              openArts={handleOpenArtsAndCrafts}
-              openResume={handleOpenResume}
-              openContact={handleOpenContact}
-              openCoding={handleOpenCoding}
-              openRecycleBin={handleOpenRecycleBin}
-              openFlashcardApp={handleOpenFlashcard}
-              openSWECards={handleOpenSWECards}
-          />
-          <PaintComp
-              show={showPaint}
-              toggle={toggleShowPaint}
-              onHelp={handleShowHelp}
-          />
-          <StreamingComp show={showStreaming} toggle={toggleShowStreaming} />
-          <ArtsAndCrafts
-              show={showArtsAndCrafts}
-              toggle={toggleShowArtsAndCrafts}
-          />
-          <Resume show={showResume} toggle={toggleShowResume} />
-          <Contact show={showContact} toggle={toggleShowContact} />
-          <Coding show={showCoding} toggle={toggleShowCoding} />
-          <Credit show={showCredit} toggle={toggleShowCredit} />
-          <Credit show={showCredit} toggle={toggleShowCredit} />
-          <SWECards show={showSWECards} toggle={toggleShowSWECards} />
-          <FlashcardApp show={showFlashcardApp} toggle={toggleShowFlashcardApp} />
-          <RecycleBin show={showRecycleBin} toggle={toggleShowRecycleBin} />
+        {showHelp && <ArtPrompt show={showHelp} toggle={handleCloseHelp} />}
 
-          {showHelp && <ArtPrompt show={showHelp} toggle={handleCloseHelp} />}
-
-          {/* Add the new section with HeroSection and DefinitionSection */}
-          <div className="main-container">
-            <HeroSection />
-            <DefinitionSection />
-          </div>
-        </WindowSizeProvider>
-      </>
+        {/* Add the new section with HeroSection and DefinitionSection */}
+        <div className="main-container">
+          <HeroSection />
+          <DefinitionSection />
+        </div>
+      </WindowSizeProvider>
+    </>
   );
 }
 
